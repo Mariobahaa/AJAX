a9f1edfd631baacb817637af82614ee9adf20376
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2020-12-12 04:23:05 +0200

    DDL1 Works

--- rockbands.js
+++ rockbands.js
@@ -12,16 +12,18 @@ request.onreadystatechange=function(){
             var body = JSON.parse(request.responseText);
             var band = checkSelection("select",0);
             var singer = checkSelection("select",1);
-            
+            console.log(band,singer);
             if(band==undefined && singer==undefined){
                 addContent("select","option",0,body);
             }
             else if(band!=undefined && singer==undefined){
                 var singerNames = [];
-                for(var singer in body[band]){
-                    singerNames.push(singer.name);
+                console.log(body[band]);
+                for(var singer=0;singer< body[band].length;singer++){
+                    //console.log((body[band][singer]).name);
+                    singerNames.push(body[band][singer]["name"]);
                 }
-                    
+                console.log(singerNames);
                 addContent("select","option",1,singerNames);
             }
             else if(band!=undefined && singer!=undefined){
@@ -34,19 +36,29 @@ request.onreadystatechange=function(){
 request.send('');
 
 function addContent(selType,elemType, ind,data){
+    //console.log(data);
     for(var itm in data){
+        //console.log(itm.val);
         var tag = document.createElement(elemType);
         tag.nodeValue = itm;
         tag.textContent= itm;
         document.querySelectorAll(selType)[ind].appendChild(tag);
+        if(selType=="select"){
+            try{
+                document.querySelectorAll(selType)[ind].firstElementChild.selected=true;
+            }
+            catch{
+                console.error("selector tag has no children!");
+            }
+        }
     }
 }
 
 function checkSelection(selType, ind){
-    var lst = document.querySelectorAll(selType)[ind].children;
-    for(var i in NodeList){
-        if(i.selected == true){
-            return i;
+    for(var i=0;i< document.querySelectorAll(selType)[ind].children.length;i++){
+        if(document.querySelectorAll(selType)[ind].children[i].selected == true){
+            console.log(document.querySelectorAll(selType)[ind].children[i].value);
+            return document.querySelectorAll(selType)[ind].children[i].value;
         }
     }
 }
